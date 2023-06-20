3f6b309f408407e81dbb2be96ace46fcc1858ee6
remiparrot <remi.parrot@gmail.com>
2023-06-20 16:21:13 +0200

    [DTLS-FUZZER][FEAT] dump both sent and received messages

---
+++ dtls-fuzzer/scripts/concretize_one.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SUT=$1			# SUT name (e.g., openssl-1.1.1) -- this name must match the name of the SUT folder inside the Docker container
+ABSFILE=$2	# Filename of the abstract sequence to concretize
+ARGFILE=$3	# Filename of the args for dtls-fuzzer
+
+if [ $ABSFILE == "wolfssl" ]; then
+	cd ${DTLS_FUZZER} && CUSTOM_FUZZ_WOLFROOT="./suts/wolfssl" LD_LIBRARY_PATH="suts/wolfssl/src/.libs:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -test ${ABSFILE}
+else
+	cd ${DTLS_FUZZER} && LD_LIBRARY_PATH="suts/${SUT}:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -test ${ABSFILE}
+fi
