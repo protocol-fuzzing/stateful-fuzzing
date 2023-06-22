3f47af4bc6e2c880873069b244da197bd30d4c6a
remiparrot <remi.parrot@gmail.com>
2023-06-22 15:56:16 +0200

    [DTLS-FUZZER][FIX] learn_automata script

--- dtls-fuzzer/scripts/learn_automata.sh
+++ dtls-fuzzer/scripts/learn_automata.sh
@@ -6,7 +6,7 @@ TESTFILE="$3"	# Mandatory test-case for the learning
 OPTIONS="$4" 	# Additional options for learning
 
 if [ "${SUT}" == "wolfssl" ]; then
-	cd ${DTLS_FUZZER} && CUSTOM_FUZZ_WOLFROOT="./suts/wolfssl" LD_LIBRARY_PATH="suts/wolfssl/src/.libs:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -timeLimit "PT48H" -equivalenceAlgorithms "SAMPLED_TESTS,RANDOM_WP_METHOD,WP_METHOD" -test ${TESTFILE} ${OPTIONS}
+	cd ${DTLS_FUZZER} && CUSTOM_FUZZ_WOLFROOT="./suts/wolfssl" LD_LIBRARY_PATH="suts/wolfssl/src/.libs:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -timeLimit "PT48H" -equivalenceAlgorithms "SAMPLED_TESTS,RANDOM_WP_METHOD,WP_METHOD" -testFile ${TESTFILE} ${OPTIONS}
 else
-	cd ${DTLS_FUZZER} && LD_LIBRARY_PATH="suts/${SUT}:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -timeLimit "PT48H" -equivalenceAlgorithms "SAMPLED_TESTS,RANDOM_WP_METHOD,WP_METHOD" -test ${TESTFILE} ${OPTIONS}
+	cd ${DTLS_FUZZER} && LD_LIBRARY_PATH="suts/${SUT}:$LD_LIBRARY_PATH" java -jar target/dtls-fuzzer.jar @${ARGSFILE} -timeLimit "PT48H" -equivalenceAlgorithms "SAMPLED_TESTS,RANDOM_WP_METHOD,WP_METHOD" -testFile ${TESTFILE} ${OPTIONS}
 fi
